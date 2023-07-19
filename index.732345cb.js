55db836cd9a741d59b2c18d6409c48464c6f8ba8
tergeltengis <tergeltengis77@gmail.com>
2023-07-19 16:57:38 +0800

    Updates

---
+++ index.732345cb.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#search");e.addEventListener("keypress",function(e){"Enter"===e.key&&(e.preventDefault(),document.getElementById("searchBtn").click())});
+//# sourceMappingURL=index.732345cb.js.map
